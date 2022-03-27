ac55f6b8dbdb515125699f4ae770eb9a24efe8af
Tamal Das <gyansujan69@gmail.com>
2022-03-28 01:44:56 +0530

    nested, simple file based routing

---
+++ pages/blogs/my-first-blog.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../../styles/Home.module.css";
+
+const my = () => {
+  return <h1 className={styles.title}>Welcome to the first blog page !!</h1>;
+};
+
+export default my;
