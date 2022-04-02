ae932a393a8ed3d11e9f2db4fb5206fb1cb02dcb
Tamal Das <gyansujan69@gmail.com>
2022-04-03 02:53:15 +0530

    fix : styled detailed issue pages

--- pages/issues/[slug].jsx
+++ pages/issues/[slug].jsx
@@ -3,6 +3,11 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react';
 import { getsingleissue } from '../../service/IssueAPi';
+import Navbar from "../../components/Navbar.jsx"
+import styleshome from "../../styles/Home.module.css"
+import styles from "../../styles/DetailedIssue.module.css"
+import Image from 'next/image';
+import profilepic from "../../public/assets/profilepic.svg"
 
 const Issuedetailed = () => {
 
@@ -27,7 +32,23 @@ const Issuedetailed = () => {
     }, [router.isReady]);
 
     return (
-        <> <div>{issuedetails && issuedetails.title}</div> </>
+        <>
+            <div className={styleshome.container}>
+                <Navbar />
+            </div>
+
+            <div className={styles.issuedetailed_parentcontainer}>
+                <div className={styles.issuedetailed_subparentcontainer}>
+                    <Image src={profilepic} width={180} height={180} />
+                    <div className={styles.issuedetailed_textdiv}>
+                        <h1 className={styles.issuedetailed_issuetitle}>{issuedetails?.title}</h1>
+                        <p className={styles.issuedetailed_issuedesc}>{issuedetails?.description}</p>
+                        <p className={styles.issuedetailed_issueauthor}>This issue was created by <span>{issuedetails?.author}</span> </p>
+                    </div>
+                </div>
+            </div>
+
+        </>
     )
 }
 
